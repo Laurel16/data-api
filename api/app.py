18f84b20d3791a69c97d4abef0fee86125007762
laure leter <laure.leter@gmail.com>
2021-06-30 17:40:39 +0200

    api prediction ok

--- api/app.py
+++ api/app.py
@@ -1,5 +1,8 @@
 
 from fastapi import FastAPI
+import pandas as pd
+import joblib
+import numpy as np
 
 app = FastAPI()
 
@@ -11,3 +14,57 @@ def index():
 
 
 # Implement a /predict endpoint
+
+@app.get("/predict")
+def predict(acousticness
+            ,danceability
+            ,duration_ms
+            ,energy
+            ,explicit
+            ,id
+            ,instrumentalness
+            ,key
+            ,liveness
+            ,loudness
+            ,mode
+            ,name
+            ,release_date
+            ,speechiness
+            ,tempo
+            ,valence
+            ,artist):
+
+    X_pred = pd.DataFrame({
+            'acousticness': float(acousticness)
+            ,'danceability': float(danceability)
+            ,'duration_ms':int(duration_ms)
+            ,'energy':float(energy)
+            ,'explicit':int(explicit)
+            ,'id': str(id)
+            ,'instrumentalness':float(instrumentalness)
+            ,'key':int(key)
+            ,'liveness':float(liveness)
+            ,'loudness':float(loudness)
+            ,'mode':int(mode)
+            ,'name':str(name)
+            ,'release_date':str(release_date)
+            ,'speechiness':float(speechiness)
+            ,'tempo':float(tempo)
+            ,'valence':float(valence)
+            ,'artist':str(artist)
+             }, index=[0])
+
+    pipeline = joblib.load('model.joblib')
+
+    prediction = pipeline.predict(X_pred)
+
+    return {
+
+              "artist": artist,
+              "name": name,
+              "popularity": prediction[0]
+            }
+
+
+
+
